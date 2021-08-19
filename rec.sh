8496dcd8014bd5be7c5b275954127c72f0807436
exwaltz <minecraftexwaltz@gmail.com>
2021-08-20 06:33:26 +0800

    Improved the ffmpeg options

--- rec.sh
+++ rec.sh
@@ -3,9 +3,8 @@ option=$(printf "%s\n%s\n%s" "rectangle" "window" "gif" | dmenu -i -p "🔴 Reco
 [ -z "$option" ] && exit
 if pidof ffmpeg
   then
-    killall ffmpeg
-
-    notify-send 'Stopped Recording!' --icon=dialog-information
+      killall ffmpeg
+      notify-send 'Stopped Recording!' --icon=dialog-information
   else 
     time=$(date +%y%b%d%H%M%S)
     case $option in
@@ -18,7 +17,20 @@ if pidof ffmpeg
 
             notify-send 'Started Recording!' --icon=dialog-information
 
-            ffmpeg -probesize 20M -f x11grab -s "$width"x"$height" -framerate 60 -thread_queue_size 512 -i $DISPLAY.0+$xpos,$ypos -pix_fmt yuv420p -vcodec libx264 -crf 28 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -tune film -qp 18 -preset ultrafast -g 30 ~/Videos/recording-$time.mp4
+            ffmpeg -probesize 20M -f x11grab -s "$width"x"$height" -fflags +genpts -re \
+                -framerate 60 -thread_queue_size 1024 -i $DISPLAY.0+$xpos,$ypos -pix_fmt yuv420p -vsync 1 \
+                -vcodec libx264 -crf 28 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" \
+                -tune film -qp 18 -preset ultrafast -g 30 -x264opts no-scenecut ./recording-$time.ts
+                # -f pulse -i 'alsa_output.pci-0000_00_0e.0.analog-stereo.monitor' \
+                # -c:a aac -af "aresample=async=1:min_hard_comp=0.100000:first_pts=0" \
+                # -map 0:v:0 -map 1:a \
+            # notify-send "Processing Video"
+            #   ffmpeg -i ~/Videos/.tmp-recording-$time.ts \
+            #       -c:v copy -c:a copy -avoid_negative_ts make_zero \
+            #       -shortest -movflags +faststart -bsf:a aac_adtstoasc -f mp4 \
+            #       ~/Videos/recording-$time.mp4
+            #   rm ~/Videos/.tmp-recording-$time.ts
+            # notify-send "Done Processing Video"
             ;;
         -g|gif)
             slop=$(slop -f "%x %y %w %h")
@@ -42,10 +54,12 @@ if pidof ffmpeg
                -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" \
                ~/Videos/.temp-video.mp4
               printf "%s" $vidTempFile
+            notify-send "Processing Video"
               ffmpeg -i  ~/Videos/.temp-video.mp4 \
-               -vf "fps=50,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" \
+               -vf "fps=30,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" \
                ~/Videos/recording-$time.gif
               rm ~/Videos/.temp-video.mp4
+            notify-send "Done Processing Video"
             fi
             ;;
 
@@ -65,10 +79,21 @@ if pidof ffmpeg
               
               # records without audio input
               # for audio add "-f alsa -i pulse" to the line below (at the end before \, without "")
-              ffmpeg -probesize 20M -f x11grab -s "$W"x"$H" -framerate 60  -thread_queue_size 512  -i $DISPLAY.0+$X,$Y \
-               -pix_fmt yuv420p -vcodec libx264 -qp 18 -preset ultrafast -tune film -g 30 -crf 28 \
-               -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" \
-               ~/Videos/recording-$time.mp4
+              ffmpeg -probesize 20M -f x11grab -s "$W"x"$H" \
+                  -framerate 60  -thread_queue_size 1024  -i $DISPLAY.0+$X,$Y \
+                  -f pulse -i 'alsa_output.pci-0000_00_0e.0.analog-stereo.monitor' -pix_fmt yuv420p -vsync 1 \
+                  -vcodec libx264 -crf 28 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" \
+                  -tune film -qp 18 -preset ultrafast -g 30 -x264opts no-scenecut \
+                  -c:a aac -af "aresample=async=1:min_hard_comp=0.100000:first_pts=0" \
+                  -map 0:v:0 -map 1:a \
+                  ~/Videos/.tmp-recording-$time.ts
+            notify-send "Processing Video"
+              ffmpeg -i ~/Videos/.tmp-recording-$time.ts \
+                  -c:v copy -c:a copy -avoid_negative_ts make_zero \
+                  -shortest -movflags +faststart -bsf:a aac_adtstoasc -f mp4 \
+                  ~/Videos/recording-$time.mp4
+              rm ~/Videos/.tmp-recording-$time.ts
+            notify-send "Done Processing Video"
             fi
             ;;
     esac
